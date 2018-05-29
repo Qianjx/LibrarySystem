599c5a73d15ce0dc25ddf9fded628eb6ecedb1e1
Qianjx <qjx10@mail.ustc.edu.cn>
2018-05-29 12:33:37 +0800

    add

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    //w.goto_ufunction_window(true);
+    w.goto_ufunction_window(true);
     return a.exec();
 }
